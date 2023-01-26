37c7972f94f916d1e240f49f35aefcdb6f17fca9
Fabio Ribeiro <fabiolopes.r@gmail.com>
2023-01-26 19:05:32 -0300

    implementa a função display_player_action

--- cli.py
+++ cli.py
@@ -4,25 +4,35 @@ from action import Action
 
 
 class CLI:
+    ACTIONS = list(Action)
+
     def get_player_action(self) -> Action:
         while True:
             try:
-                choices = [f"[{i + 1}]{action}" for i, action in enumerate(Action)]
-                selection = int(input(f"Enter a choice {choices}:"))
-
-                if selection in range(1, len(choices) + 1):
-                    return list(Action)[selection - 1]
+                selection = int(input("Enter a choice: "))
+                if selection in range(1, len(self.ACTIONS) + 1):
+                    return self.ACTIONS[selection - 1]
                 else:
                     print("That was no valid number. Try again.")
             except ValueError:
                 print("You entered something other than a number. Try again.")
 
     def get_cpu_action(self) -> Action:
-        return random.choice(list(Action))
+        return random.choice(self.ACTIONS)
 
-    def display_rules(self):
+    def display_player_action(self) -> None:
+        choices = [f"[{i + 1}]{action}" for i, action in enumerate(Action)]
+        print(f"Valid actions: {choices}")
+
+    def display_rules(self) -> None:
         pass
+
+    def display_opening_message(self) -> None:
+        print("****************************************************")
+        print("Welcome to the Rock, Paper, Scissors, Lizard, Spock!")
+        print("****************************************************")
 
 
 cli = CLI()
+cli.display_player_action()
 cli.get_player_action()
