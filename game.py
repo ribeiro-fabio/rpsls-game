2ce5c71198b1c6bd023c85a55fdafd817dd44456
Fabio Ribeiro <fabiolopes.r@gmail.com>
2023-02-10 20:17:10 -0300

    refatora métodos play e turn

--- game.py
+++ game.py
@@ -3,38 +3,42 @@ from rules import Rules
 
 
 class Game:
-    def __init__(self) -> None:
-        self.cli = CLI()
-        self.rules = Rules()
+    rules = Rules()
+    cli = CLI()
+
+    def __init__(self):
+        self.player_name = ""
+        self.cpu = "CPU"
 
     def turn(self):
-        # separar lógica do jogo da chamada do jogo
-        player_name = self.cli.get_player_name()
-        cpu = "CPU"
 
         self.cli.display_player_action()
 
         player_action = self.cli.get_player_action()
         cpu_action = self.cli.get_cpu_action()
 
-        winner = self.rules.get_winner(player_action, cpu_action)
-        self.cli.display_round(player_name, cpu, player_action, cpu_action)
+        winner = Rules.get_winner(player_action, cpu_action)
+        self.cli.display_round(self.player_name, self.cpu, player_action, cpu_action)
 
         if winner == player_action:
-            self.cli.display_winner(player_name, winner)
+            self.cli.display_winner(self.player_name, winner)
         elif winner == cpu_action:
-            self.cli.display_winner(cpu, winner)
+            self.cli.display_winner(self.cpu, winner)
         else:
             self.cli.display_tie()
 
     def play(self):
         self.cli.display_opening_message()
         self.cli.display_rules()
-        turns = 3
-        try:
-                play_again = input("Play again? (y/n): ")
-                print("")
-                if play_again.lower() != "y":
-                    break
-            except ValueError:
-                print("Please enter y or n.")
+        self.player_name = self.cli.get_player_name()
+        self.turn()
+
+        while True:
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() not in ("y", "n"):
+                print("Invalid input. Enter y or n")
+            elif play_again.lower() == "y":
+                self.turn()
+            else:
+                print("Goodbye!")
+                break
